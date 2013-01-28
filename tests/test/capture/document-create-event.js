a951dfc143347e3f62fe87e53de7a47f2bdc9d80
Lon Ingram <lawnsea@gmail.com>
2013-01-27 18:06:03 -0600

    Mark events created by `document.createEvent` as synthetic

---
+++ tests/test/capture/document-create-event.js
@@ -0,0 +1,41 @@
+/* vim: set et ts=2 sts=2 sw=2: */
+
+var expect = require('expect.js');
+
+describe('Reanimator interposes document.createEvent', function () {
+  var build = require('../../util/hooks').build;
+  var url = 'http://localhost:' + process.env.FIXTURE_PORT + '/index.html';
+  var driver;
+
+  beforeEach(function (done) {
+    build().then(function (builtDriver) {
+      driver = builtDriver;
+      done();
+    });
+  });
+
+  afterEach(function () {
+    driver.quit();
+  });
+
+  it('sets `_reanimator.synthetic` to true on created events', function (done) {
+    driver.get(url).
+      then(function () {
+        return driver.executeScript(function () {
+          Reanimator.capture();
+
+          expected = Reanimator.util.event.serialization.
+            serialize(document.createEvent('HTMLEvents'));
+          
+          Reanimator.cleanUp();
+          return JSON.stringify(expected);
+        });
+      }).
+      then(function (result) {
+        result = JSON.parse(result);
+
+        expect(result._reanimator).to.eql({ synthetic: true });
+        done();
+      });
+  });
+});
